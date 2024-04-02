76029555373afb2f09d1bd8e0d58e8a0ca3a5051
Asela Wadugedara <aselap@treinetic.com>
2024-04-02 16:10:13 +0530

    Project created

---
+++ Social Login/Social Login/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Social Login
+//
+//  Created by Asela Wadugedara on 2024-04-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button("Login Fb") {
+                
+            }
+            Button("Login Apple") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Social Login/Social Login/Social_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_LoginApp.swift
+//  Social Login
+//
+//  Created by Asela Wadugedara on 2024-04-02.
+//
+
+import SwiftUI
+
+@main
+struct Social_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
